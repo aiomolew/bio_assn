e2a8f56c3ef96594c84391cfe199cab6252698b6
Abigael Omolewu <aomolewu@localhost.localdomain>
2020-04-07 01:47:03 -0400

    assn06-add agrparse to accept GFF and FASTA files

--- parseGFF.py
+++ parseGFF.py
@@ -1,22 +1,42 @@
 #! /usr/bin/env python3
 import csv
+from Bio import SeqIO
+import argparse
+
+# create an argument parser object
+parser = argparse.ArgumentParser(description = "This script parse GFF and Fasta files and does stuffs with them")
+
+# add positional argument for teh input position in the Fib sequence
+parser.add_argument("gff", help="Name of GFF file")
+parser.add_argument("fasta", help="Name of the FASTA file")
+
+# parse the arguments
+args = parser.parse_args()
+
 #specify input file
-gff_file ='watermelon.gff'
+gff_file = args.gff
+fasta_file =  args.fasta
+
+#read and parse the FASTA file
+genome = SeqIO.read(fasta_file, 'fasta')
+
 
 # Create an emptylist gene_name
 gene_names =[]
 # Read GFF file line by line
-with open(gff_file, 'r') as gff:
+with open(gff_file, 'r') as gff_input:
 	
 	# create a csv reader object
-	reader = csv.reader(gff, delimiter="\t")
+	reader = csv.reader(gff_input, delimiter="\t")
 	
 	for line in reader:
 		#skip blank lines
 		if not line:
 			continue;
 		else:
-			print(line[3], line[4])
+			start = line[3]
+			end = line[4]
+			print(start, end)
 
 
 
